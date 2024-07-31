0dad35abaacd9126ffa8413be3f76cf7025ec231
jerom <jerom@DESKTOP-UTIHCDT>
2024-07-31 06:59:33 -0400

    Add project files.

---
+++ ConsoleApp.OutPutDemo/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//WriteLine is a method that takes a block of text and print it to the screen.
+//It breaks the line after it prints the text. 
+Console.WriteLine("Hello, World! My name is Jeromé Charles");
+Console.WriteLine("I am from Grenada");
+
+//Write is a method that takes a block of text and print it to the screen
+//It does not break the line after it prints the text.
+Console.Write("Line 1");
+Console.Write("Line 2");
---
+++ ConsoleApp.VariablesDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Different Datatypes
+/*
+    text - string
+    integers - int
+    decimal - double, float, decimal
+    logical - bool
+ */
+
+string name = "Jerome Charles";
+
+Console.WriteLine(name);
+Console.WriteLine("I am " + name); // String Concatenation
+Console.WriteLine($"They call me {name}");// String Interpolation
+Console.WriteLine("I was given the name {0}", name);// Formatted String
+
+int age = 26;
+int retirementYearsLeft = 39;
+int retirementAge = age + retirementYearsLeft;
+
+Console.WriteLine("My age is: " + age);
+Console.WriteLine("My retirement age is: " + retirementAge);
+
+bool isRetired = false;
+
+Console.WriteLine("Am i retired? " + isRetired);
+
